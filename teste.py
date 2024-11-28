029876f4fede16cb99019dd42fe9995a30fdadc1
luizfernandodag <luizfernandodag@gmail.com>
2024-11-27 21:07:34 -0300

    first commit

---
+++ teste.py
@@ -0,0 +1,11 @@
+print("Hello")
+
+nome = 'Luiz'
+profissao = "Programador"
+print(nome, profissao)
+
+email = input('Digite seu email:')
+senha = input('Digite sua senha:')
+
+print("email:", email)
+print("senha:", senha)
